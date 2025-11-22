88f10a1a869861504837b954167dbdf1b9d17192
GuameDev <pablommartinezdev@gmail.com>
2025-11-22 17:37:51 +0100

    fix: cast type on BinaryFileReader

--- src/main/java/es/cifpcarlos3/file/readers/BinaryFileReader.java
+++ src/main/java/es/cifpcarlos3/file/readers/BinaryFileReader.java
@@ -4,6 +4,13 @@ import java.io.*;
 import java.nio.file.Path;
 public class BinaryFileReader<T> implements FileReader<T> {
 
+
+    private final Class<T> type;
+
+    public BinaryFileReader(Class<T> type) {
+        this.type = type;
+    }
+
     @Override
     public T read(Path filePath) {
         System.out.println("Leyendo el fichero " + filePath.getFileName() + "...");
@@ -11,8 +18,8 @@ public class BinaryFileReader<T> implements FileReader<T> {
         try (FileInputStream fileInputStream = new FileInputStream(filePath.toFile());
              ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
 
-            return (T) objectInputStream.readObject();
-
+            var data = objectInputStream.readObject();
+            return type.cast(data);
 
         } catch (IOException | ClassNotFoundException e) {
             throw new RuntimeException("Error leyendo fichero binario " + filePath, e);
